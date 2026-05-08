bc7e605203415fb5585371d13697363aa3f14835
sridivya1puttamreddy <Sridivya@finstack-tech.com>
2026-05-08 09:17:24 +0530

    Cors middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,13 +7,19 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+origins = [
+    "https://team-task-manager-3v3w-7i3swpbbp-sridivyaputtamreddys-projects.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(auth_routes.router)
 app.include_router(project_routes.router)
 app.include_router(task_routes.router)
